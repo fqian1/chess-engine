82c3d0c19ee4609ad126d2f85c4351810dc2a40b
fqian <francois.qian2@gmail.com>
2026-01-17 15:01:24 +0000

    added nix for reproducibility

--- shell.nix
+++ shell.nix
@@ -1,12 +1,18 @@
-{pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-  buildInputs = [
-    pkgs.cargo
-    pkgs.clippy
-    pkgs.rustc
-    pkgs.rust-analyzer
-  ];
-  shellHook = ''
-    echo "rust shell loaded"
-  '';
-}
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> {overlays = [rust_overlay];};
+  rustNightly = pkgs.rust-bin.nightly.latest.default.override {
+    extensions = ["rust-src" "rust-analyzer"];
+  };
+in
+  pkgs.mkShell {
+    buildInputs = [
+      rustNightly
+      pkgs.cargo
+    ];
+
+    shellHook = ''
+      echo "Rust nightly shell loaded"
+      rustc --version
+    '';
+  }
