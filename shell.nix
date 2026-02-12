83dba5321049c9f00f7dcfc5bf69fcfba09dc668
fqian <francois.qian2@gmail.com>
2026-02-12 12:42:46 +0000

    add burn

--- shell.nix
+++ shell.nix
@@ -7,12 +7,25 @@ let
 in
   pkgs.mkShell {
     buildInputs = [
+      pkgs.rustup
+      pkgs.pkg-config
+      pkgs.openssl
       rustNightly
       pkgs.cargo
     ];
-
     shellHook = ''
-      echo "Rust nightly shell loaded"
-      rustc --version
+      LD_LIBRARY_PATH="''${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}${
+        with pkgs;
+          lib.makeLibraryPath [
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ]
+      }"
+      export LD_LIBRARY_PATH
+        echo "Rust nightly shell loaded"
+        rustc --version
     '';
   }
