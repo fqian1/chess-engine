6c8da931bbc03fc5af126fec49e143de7ddeac73
fqian <francois.qian2@gmail.com>
2026-03-06 08:55:36 +0000

    feat: data structure for self play

--- src/data.rs
+++ src/data.rs
@@ -1,59 +1,73 @@
-use burn::tensor::{Tensor, backend::Backend};
+use burn::{
+    data::dataloader::{DataLoaderBuilder, batcher::Batcher},
+    tensor::{Int, Tensor, backend::Backend},
+};
+use rand::{rngs::SmallRng, seq::IndexedRandom};
+
+#[derive(Default, Clone)]
+pub struct ChessBatcher {}
 
 #[derive(Clone, Debug)]
 pub struct TrainingSample {
-    pub board: [f32; 64 * 14],
-    pub meta: [f32; 5],
-    pub target_policy: [f32; 64],
-    pub target_value: f32,
+    pub board: [f32; 64 * 14],    // 64 * 14 boards
+    pub meta: [f32; 5],           // 4 castling rights 1 hot + f32 move counter
+    pub target_policy: [f32; 64], // distribution over squares
+    pub target_value: i32,        // distribution over w/d/l
 }
 
-pub struct Batch<B: Backend> {
-    pub boards:  Tensor<B, 3>,
-    pub meta:    Tensor<B, 2>,
-    pub targets: Tensor<B, 2>,
-    pub values:  Tensor<B, 1>,
+#[derive(Clone, Debug)]
+pub struct ChessBatch<B: Backend> {
+    pub boards: Tensor<B, 3>,             // Batch x 64 x 14
+    pub metas: Tensor<B, 2>,              // Batch x 5
+    pub policy_targets: Tensor<B, 2>,     // Batch x 64
+    pub value_targets: Tensor<B, 1, Int>, // Batch x 3
+}
+
+impl<B: Backend> Batcher<B, TrainingSample, ChessBatch<B>> for ChessBatcher {
+    fn batch(&self, items: Vec<TrainingSample>, device: &B::Device) -> ChessBatch<B> {
+        let n = items.len();
+
+        let mut boards = Vec::with_capacity(n * 64 * 14);
+        let mut metas = Vec::with_capacity(n * 5);
+        let mut targets = Vec::with_capacity(n * 64);
+        let mut values = Vec::with_capacity(n * 3);
+
+        for item in items {
+            boards.extend_from_slice(&item.board);
+            metas.extend_from_slice(&item.meta);
+            targets.extend_from_slice(&item.target_policy);
+            values.push(item.target_value)
+        }
+
+        ChessBatch {
+            boards: Tensor::<B, 1>::from_floats(boards.as_slice(), device).reshape([n, 64, 14]),
+            metas: Tensor::<B, 1>::from_floats(metas.as_slice(), device).reshape([n, 5]),
+            policy_targets: Tensor::<B, 1>::from_floats(targets.as_slice(), device).reshape([n, 64]),
+            value_targets: Tensor::<B, 1, Int>::from_ints(values.as_slice(), device).reshape([n]),
+        }
+    }
 }
 
 pub struct ReplayBuffer {
     capacity: usize,
+    pointer:  usize,
+    rng:      SmallRng,
     buffer:   Vec<TrainingSample>,
 }
 
 impl ReplayBuffer {
     pub fn new(capacity: usize) -> Self {
-        Self { capacity, buffer: Vec::with_capacity(capacity) }
+        Self { capacity, pointer: 0, buffer: Vec::with_capacity(capacity), rng: rand::make_rng() }
     }
 
     pub fn push(&mut self, sample: TrainingSample) {
-        if self.buffer.len() >= self.capacity {
-            self.buffer.remove(0);
-        }
-        self.buffer.push(sample);
+        self.buffer[self.pointer] = sample;
+        self.pointer = (self.pointer + 1) % self.capacity;
     }
 
-    pub fn sample_batch<B: Backend>(&self, batch_size: usize, device: &B::Device) -> Batch<B> {
-        // top N
-        let start = self.buffer.len().saturating_sub(batch_size);
-        let samples = &self.buffer[start..];
-
-        let mut flat_boards = Vec::with_capacity(samples.len() * 64 * 14);
-        let mut flat_meta = Vec::with_capacity(samples.len() * 5);
-        let mut targets = Vec::with_capacity(samples.len() * 64);
-        let mut values = Vec::with_capacity(samples.len());
-
-        for s in samples {
-            flat_boards.extend_from_slice(&s.board);
-            flat_meta.extend_from_slice(&s.meta);
-            targets.extend_from_slice(&s.target_policy);
-            values.push(s.target_value);
-        }
-
-        Batch {
-            boards:  Tensor::<B, 3>::from_floats(flat_boards.as_slice(), device).reshape([samples.len(), 64, 14]),
-            meta:    Tensor::<B, 2>::from_floats(flat_meta.as_slice(), device).reshape([samples.len(), 5]),
-            targets: Tensor::<B, 2>::from_floats(targets.as_slice(), device).reshape([samples.len(), 64]),
-            values:  Tensor::from_floats(values.as_slice(), device),
-        }
+    pub fn sample_batch<B: Backend>(&mut self, batch_size: usize, device: &B::Device) -> ChessBatch<B> {
+        let samples: Vec<TrainingSample> = self.buffer.sample(&mut self.rng, batch_size).cloned().collect();
+        let batcher = ChessBatcher {};
+        batcher.batch(samples, device)
     }
 }
