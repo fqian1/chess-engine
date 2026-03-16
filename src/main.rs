5c75a8ed7510af259458ba0739162153e0283e90
fqian <francois.qian2@gmail.com>
2026-03-16 01:42:54 +0000

    clean up comments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use burn::backend::{Autodiff, Wgpu};
+use burn::module::AutodiffModule;
+use chess_engine::data::TrainingSample;
 use chess_engine::*;
 use std::io;
 use std::io::Write;
-use burn::backend::Wgpu;
 
 pub struct TrainingMetrics {
     pub epoch: u32,
-    pub illegal_moves_generate: u32,
+    pub illegal_moves_generated: u32,
     pub total_moves_generated: u32,
     pub average_loss: u32,
     pub average_game_length: u32,
@@ -19,37 +21,30 @@ pub fn display_moves(moves: &Vec<ChessMove>) {
 }
 
 fn main() {
-    // type MyBackend = Wgpu<f32, i32>;
+    type MyInferenceBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyInferenceBackend>;
+
+    let device = burn::backend::Wgpu::default();
+    let artifact_dir = "tmp/stats";
+    let mut optimizer = config.optimizer.init();
 
     // let size = model_size * 4;
     // let n_heads = size;
     // let n_layers = size;
     // let d_model = n_heads * 64;
     // let d_ff = 4 * d_model;
-    //
 
-    let mut game = ChessGame::default();
-    game.zobrist_hash = game.calculate_hash();
-
-    let mut chessgames: Vec<ChessGame> = vec![ChessGame::default(); 80];
+    let mut training_data: Vec<TrainingSample> = Vec::with_capacity(30000);
+    let mut chessgames: Vec<ChessGame> = vec![ChessGame::default(); 1024];
     chessgames.iter_mut().for_each(|game| {
-        game.zobrist_hash = game.calculate_hash()
+        game.zobrist_hash = game.calculate_hash();
+        game.rule_set = chess_game::RuleSet::Legal;
     });
 
-    let moves: Vec<ChessMove> = game.generate_pseudolegal().into_iter().collect();
-    display_moves(&moves);
-    println!("{}", game.chessboard.display_ascii());
-    // loop
-    // 1. convert to transformer representation
-    // 2. check game state
-    // 3. run inference, collect from squares
-    // 4. run inference, collect to squares
-    // 5. run inference, collect promotion square
-    // 6. generate ~100 candidate moves, order by from score, to score
-    // 7. generate moves
-    // 8. check move validity
-    // 9.
-    // 10.
+    loop {
+        let model = config.model.init::<B>(&device, true);
+        chessgames.iter_mut().for_each(|game| game.outcome = game.check_game_state());
+    }
 
     // loop {
     //     ChessGame::fen_to_ascii(&game.to_fen());
