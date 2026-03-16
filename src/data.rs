53609e0691d8d06a0a6d81d01b640de24771fb04
fqian <francois.qian2@gmail.com>
2026-03-16 01:42:04 +0000

    fix tensor shapes

--- src/data.rs
+++ src/data.rs
@@ -1,32 +1,32 @@
 use burn::{
     data::dataloader::batcher::Batcher,
-    tensor::{Int, Tensor, backend::Backend},
+    tensor::{Tensor, backend::Backend},
 };
 use rand::{rngs::SmallRng, seq::IndexedRandom};
 
 #[derive(Default, Clone)]
 pub struct ChessBatcher {}
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct TrainingSample {
     pub board: [f32; 64 * 14],    // 64 * 14 boards
     pub meta: [f32; 5],           // 4 castling rights 1 hot + f32 move counter
     pub target_policy: [f32; 64], // distribution over squares
-    pub target_value: i32,        // distribution over w/d/l (but target is one hot so i32)
+    pub target_value: [f32; 3],   // distribution over w/d/l (cant use Int because search depth non terminal)
 }
 
 impl Default for TrainingSample {
     fn default() -> Self {
-        TrainingSample { board: [0.0; 64 * 14], meta: [0.0; 5], target_policy: [0.0; 64], target_value: 0 }
+        TrainingSample { board: [0.0; 64 * 14], meta: [0.0; 5], target_policy: [0.0; 64], target_value: [0.0; 3] }
     }
 }
 
 #[derive(Clone, Debug)]
 pub struct ChessBatch<B: Backend> {
-    pub boards: Tensor<B, 3>,             // Batch x 64 x 14
-    pub metas: Tensor<B, 2>,              // Batch x 5
-    pub policy_targets: Tensor<B, 2>,     // Batch x 64
-    pub value_targets: Tensor<B, 1, Int>, // Batch x 1
+    pub boards: Tensor<B, 3>,         // Batch x 64 x 14
+    pub metas: Tensor<B, 2>,          // Batch x 5
+    pub policy_targets: Tensor<B, 2>, // Batch x 64
+    pub value_targets: Tensor<B, 2>,  // Batch x 1
 }
 
 impl<B: Backend> Batcher<B, TrainingSample, ChessBatch<B>> for ChessBatcher {
@@ -36,20 +36,20 @@ impl<B: Backend> Batcher<B, TrainingSample, ChessBatch<B>> for ChessBatcher {
         let mut boards = Vec::with_capacity(n * 64 * 14);
         let mut metas = Vec::with_capacity(n * 5);
         let mut targets = Vec::with_capacity(n * 64);
-        let mut values = Vec::with_capacity(n);
+        let mut values = Vec::with_capacity(n * 3);
 
         for item in items {
             boards.extend_from_slice(&item.board);
             metas.extend_from_slice(&item.meta);
             targets.extend_from_slice(&item.target_policy);
-            values.push(item.target_value)
+            values.extend_from_slice(&item.target_value)
         }
 
         ChessBatch {
             boards: Tensor::<B, 3>::from_floats(boards.as_slice(), device).reshape([n, 64, 14]),
             metas: Tensor::<B, 2>::from_floats(metas.as_slice(), device).reshape([n, 5]),
             policy_targets: Tensor::<B, 2>::from_floats(targets.as_slice(), device).reshape([n, 64]),
-            value_targets: Tensor::<B, 1, Int>::from_ints(values.as_slice(), device).reshape([n]),
+            value_targets: Tensor::<B, 2>::from_floats(values.as_slice(), device).reshape([n, 3]),
         }
     }
 }
@@ -65,28 +65,11 @@ impl ReplayBuffer {
         Self { capacity, pointer: 0, buffer: Vec::with_capacity(capacity) }
     }
 
-    // for inferencing!!
-    pub fn to_tensor<B: Backend>(&self, device: &B::Device) -> (Tensor<B, 3>, Tensor<B, 2>) {
-        let n = self.buffer.len();
-
-        let mut boards = Vec::with_capacity(n * 64 * 14);
-        let mut metas = Vec::with_capacity(n * 5);
-
-        for item in &self.buffer {
-            boards.extend_from_slice(&item.board);
-            metas.extend_from_slice(&item.meta);
-        }
-
-        let t1 = Tensor::<B, 3>::from_floats(boards.as_slice(), device).reshape([n, 64, 14]);
-        let t2 = Tensor::<B, 2>::from_floats(metas.as_slice(), device).reshape([n, 5]);
-        (t1, t2)
-    }
-
-    pub fn push(&mut self, sample: TrainingSample) {
+    pub fn push(&mut self, sample: &TrainingSample) {
         if self.buffer.len() < self.capacity {
-            self.buffer.push(sample);
+            self.buffer.push(sample.clone());
         } else {
-            self.buffer[self.pointer] = sample;
+            self.buffer[self.pointer] = sample.clone();
             self.pointer = (self.pointer + 1) % self.capacity;
         }
     }
