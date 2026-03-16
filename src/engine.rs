30864d908b1d19f8306a442f4654ca5082b16ca0
fqian <francois.qian2@gmail.com>
2026-03-16 01:44:43 +0000

    wip: Mon, 16 Mar 2026 01:44:43 +0000

--- src/engine.rs
+++ src/engine.rs
@@ -1,23 +1,23 @@
 use burn::{
     Tensor,
     config::Config,
-    optim::{Adam, AdamConfig, AdamW, GradientsParams, Optimizer, SimpleOptimizer, adaptor::OptimizerAdaptor},
+    optim::{Adam, AdamConfig, GradientsParams, Optimizer, adaptor::OptimizerAdaptor},
     prelude::Backend,
-    tensor::{Bool, backend::AutodiffBackend},
-    train::SupervisedTraining,
+    tensor::{Bool, TensorData, activation::softmax, backend::AutodiffBackend},
 };
 use rand::rngs::SmallRng;
 
 use crate::{
     ChessGame, ChessSquare, ChessTransformer, Color, ReplayBuffer,
-    chess_game::Outcome,
-    data::{ChessBatch, ChessBatcher, TrainingSample},
+    chess_game::{Outcome, RuleSet},
+    data::{ChessBatch, TrainingSample},
     model::ChessTransformerConfig,
 };
 
 #[derive(Config, Debug)]
 pub struct TrainingConfig {
     pub model: ChessTransformerConfig,
+    pub masked: bool,
     pub optimizer: AdamConfig,
     #[config(default = 10)]
     pub num_epochs: usize,
@@ -36,110 +36,202 @@ fn create_artifact_dir(artifact_dir: &str) {
     std::fs::create_dir_all(artifact_dir).ok();
 }
 
-pub fn train_model<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, masked: bool, device: &B::Device) {
-    let mut model: ChessTransformer<B> = config.model.init(device, masked);
-    let mut optimizer = config.optimizer.init::<B, ChessTransformer<B>>();
+pub fn to_tensor<B: Backend>(
+    buffer: &Vec<([f32; 64 * 14], [f32; 5])>,
+    device: &B::Device,
+) -> (Tensor<B, 3>, Tensor<B, 2>) {
+    let n = buffer.len();
+
+    let mut boards = Vec::with_capacity(n * 64 * 14);
+    let mut metas = Vec::with_capacity(n * 5);
+
+    for item in buffer {
+        boards.extend_from_slice(&item.0);
+        metas.extend_from_slice(&item.1);
+    }
+
+    let t1 = Tensor::<B, 3>::from_floats(boards.as_slice(), device).reshape([n, 64, 14]);
+    let t2 = Tensor::<B, 2>::from_floats(metas.as_slice(), device).reshape([n, 5]);
+    (t1, t2)
+}
+
+pub fn to_f32(
+    game: &ChessGame,
+    from_sq: Option<&ChessSquare>,
+    to_sq: Option<&ChessSquare>,
+) -> ([f32; 64 * 14], [f32; 5]) {
+    let (chess_board, castling_rights, ep_sq) = if game.side_to_move == Color::White {
+        (game.chessboard.clone(), game.castling_rights, game.en_passant)
+    } else {
+        (
+            game.chessboard.flip_board(),
+            game.castling_rights.flip_perspective(),
+            game.en_passant.map(|x| x.square_opposite()),
+        )
+    };
+
+    let mut data = [0f32; 64 * 14];
+    chess_board.pieces[0][0].write_to_slice(&mut data[0..64]);
+    chess_board.pieces[0][1].write_to_slice(&mut data[64..128]);
+    chess_board.pieces[0][2].write_to_slice(&mut data[128..192]);
+    chess_board.pieces[0][3].write_to_slice(&mut data[192..256]);
+    chess_board.pieces[0][4].write_to_slice(&mut data[256..320]);
+    chess_board.pieces[0][5].write_to_slice(&mut data[320..384]);
+
+    chess_board.pieces[1][0].write_to_slice(&mut data[384..448]);
+    chess_board.pieces[1][1].write_to_slice(&mut data[448..512]);
+    chess_board.pieces[1][2].write_to_slice(&mut data[512..576]);
+    chess_board.pieces[1][3].write_to_slice(&mut data[576..640]);
+    chess_board.pieces[1][4].write_to_slice(&mut data[640..704]);
+    chess_board.pieces[1][5].write_to_slice(&mut data[704..768]);
+
+    if let Some(square) = ep_sq {
+        data[768 + square.0 as usize] = 1.0;
+    }
+
+    if let Some(square) = from_sq {
+        data[832 + square.0 as usize] = 1.0;
+    }
+
+    if let Some(square) = to_sq {
+        data[832 + square.0 as usize] = 1.0;
+    }
+
+    let mut meta = [0f32; 5];
+    for i in 0..4 {
+        meta[i] = (castling_rights.0 >> i & 1).into();
+    }
+    meta[4] = game.halfmove_clock as f32 / 50.0;
+
+    (data, meta)
+}
+
+pub fn train_model<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, device: &B::Device) {
+    let model: ChessTransformer<B> = config.model.init(device);
+    let optimizer = config.optimizer.init::<B, ChessTransformer<B>>();
     let mut replay_buffer = ReplayBuffer::new(config.batch_size * 64);
 
     B::seed(device, config.seed);
+    let default_state = to_f32(&ChessGame::default(), None, None);
     let mut counter = 0;
     let mut games = vec![ChessGame::default(); config.batch_size];
-    games.iter_mut().for_each(|game| game.zobrist_hash = game.calculate_hash());
-    let mut samples = vec![TrainingSample::default(); config.batch_size];
+    let mut samples: Vec<TrainingSample> = Vec::with_capacity(config.batch_size);
+    let mut current_game_state: Vec<([f32; 64 * 14], [f32; 5])> = Vec::with_capacity(config.batch_size);
+    games.iter_mut().for_each(|game| {
+        game.zobrist_hash = game.calculate_hash();
+        let state = to_f32(&game, None, None);
+        current_game_state.push(state);
+    });
 
     loop {
         println!("--- Iteration: {} ---", &counter);
-        // why do i even store outcome in chess game lol
-        games.iter_mut().enumerate().for_each(|(i, game)| {
-            match game.check_game_state() {
-                Outcome::Finished(color) => {
-                    match color {
-                        Some(Color::White) => {
-                            game.outcome = Outcome::Finished(Some(Color::White));
-                            samples[i].target_value = 0;
-                        }
-                        Some(Color::Black) => {
-                            game.outcome = Outcome::Finished(Some(Color::Black));
-                            samples[i].target_value = 2;
-                        }
-                        None => {
-                            game.outcome = Outcome::Finished(None);
-                            samples[i].target_value = 1;
-                        }
-                    }
-                    replay_buffer.push(samples[i]);
-                    *game = ChessGame::default();
-                }
+        let mut masks = vec![false; 64 * config.batch_size];
 
-                Outcome::Unfinished => {
-                    let (board, meta) = game.to_f32(); // this already flips the board if black!!!
-                    replay_buffer.buffer[i].board = board;
-                    replay_buffer.buffer[i].meta = meta;
-                    // need to collect
+        // if game is over, push value to samples, push sample to buffer, start new game, generate moves
+        games.iter_mut().enumerate().for_each(|(i, game)| {
+            if let Outcome::Finished(color) = game.check_game_state() {
+                match color {
+                    Some(Color::White) => {
+                        game.outcome = Outcome::Finished(Some(Color::White));
+                        samples[i].target_value = [1.0, 0.0, 0.0];
+                    }
+                    Some(Color::Black) => {
+                        game.outcome = Outcome::Finished(Some(Color::Black));
+                        samples[i].target_value = [0.0, 0.0, 1.0];
+                    }
+                    None => {
+                        game.outcome = Outcome::Finished(None);
+                        samples[i].target_value = [0.0, 1.0, 0.0];
+                    }
                 }
+                replay_buffer.push(&samples[i]);
+                *game = ChessGame::default();
+                current_game_state[i] = default_state;
             }
+            let mut moves = game.generate_pseudolegal();
+            if game.rule_set == RuleSet::Legal {
+                moves.retain(|mv| game.is_legal(mv));
+            }
+            moves.into_iter().for_each(|mv| masks[i * 64 + mv.from.0 as usize] = true);
         });
+
+        let masks = if config.masked { Some(masks) } else { None };
+
+        // vec (u8, f32); 64, (f32; 3)
+        let outs = step(model.clone(), &current_game_state, config.batch_size, masks, device);
+
+        // pick top square, replace with mcst eventually
+        let mut from_squares: Vec<ChessSquare> = Vec::with_capacity(config.batch_size);
+        for (policies, _) in outs {
+            let (sq, _) = policies.iter().max_by(|&a, &b| a.1.total_cmp(&b.1)).expect("engine bug");
+            from_squares.push(ChessSquare::new(*sq).unwrap());
+        }
+
+        // mcst naturally makes legal target policy, illegal move culled, visit count -> 0.0.
+        // difference is, whether or not itll be propogated back.
+        // let (target_policy, target_value) = mcst::run();
+
+        samples.iter_mut().enumerate().for_each(|(i, sample)| {
+            sample.board = current_game_state[i].0;
+            sample.meta = current_game_state[i].1;
+            //sample.target_policy = policy_from_mcst
+            //sample.target_value = value_from_mcst
+        });
+        replay_buffer.buffer.append(&mut samples);
+
+        // create new samples
+        let mut samples: Vec<TrainingSample> = Vec::with_capacity(config.batch_size);
+
+        let to_sqs: Vec<u8> = Vec::with_capacity(config.batch_size);
+        // wait... repeating...
 
         counter += 1;
     }
 }
 
-fn get_square<B: Backend>(
+fn step<B: Backend>(
     model: ChessTransformer<B>,
-    replay_buffer: &ReplayBuffer,
-    masked: Option<Vec<[bool;64]>>,
-    temperature: f32,
+    games: &Vec<([f32; 64 * 14], [f32; 5])>,
+    batch_size: usize,
+    mask: Option<Vec<bool>>,
     device: &B::Device,
-) -> Vec<u8> {
-    let (board, meta) = replay_buffer.to_tensor::<B>(device);
-    let (policy, value) = model.forward(board, meta);
-    if let Some(bools) = masked {
-        let mask = Tensor::<B, 2, Bool>::from_bool(bools.into(), device);
-        policy.mask_fill(mask, -1e9);
+    // eww why the fuck am i passing raw data around like this
+) -> Vec<([(u8, f32); 64], (f32, f32, f32))> {
+    // replay buffer must be populated by chess game before calling step
+    let (board, meta) = to_tensor(games, device);
+    let (mut policy, value) = model.forward(board, meta);
+    if let Some(bools) = mask {
+        let data = TensorData::new(bools, [batch_size, 64]); // scary
+        let mask = Tensor::<B, 2, Bool>::from_data(data, device);
+        policy = policy.clone().mask_fill(mask, -1e9);
     }
-    let [batch_size, _] = policy.dims();
+    policy = softmax(policy, 1);
     // batch x 64
-    let t_board: Tensor<B, 1> = policy.flatten(1, 2);
-    let floats = t_board.into_data().to_vec::<f32>().unwrap();
+    let policy: Tensor<B, 1> = policy.flatten(0, 1);
+    let policy = policy.into_data().to_vec::<f32>().unwrap();
+    let values: Tensor<B, 1> = value.flatten(0, 1);
+    let values = values.into_data().to_vec::<f32>().unwrap();
 
-    let mut squares = vec![0; 64 * batch_size];
+    let mut out = vec![([(0, 0.0); 64], (0.0, 0.0, 0.0))];
+
     for i in 0..batch_size {
+        let mut temp = [(0, 0.0); 64];
+        let mut temp2 = (0.0, 0.0, 0.0);
         for j in 0..64 {
-            squares[i + j] = j as u8;
+            temp[j] = (j as u8, policy[j + i * 64]);
+            temp2.0 = values[j];
+            temp2.1 = values[j + 1];
+            temp2.2 = values[j + 2];
         }
-        let start = i * 64;
-        let end = start + 64;
-        let slice = &mut squares[start..end];
-        slice.sort_unstable_by(|&a, &b| floats[a as usize].total_cmp(&floats[b as usize]));
+        out[i] = (temp, temp2);
     }
 
-    squares
-
-    //         let policy_sq = match rule_set {
-    //                 legal => {
-    //                         let mut indices: [usize; 64] = [0; 64];
-    //                         for i in 0..64 {
-    //                                 squares[i] = i
-    //                         }
-    //
-    //                         indices.sort_unstable_by(|&a, &b| {value[b].total_cmp(&value[a])})
-    //
-    //                         squares: Vec<ChessSquare> = indices.iter().map(|x| ChessSquare::from(x).unwrap())
-    //                         squares.filter(self.legal_from_sq)
-    //                         top_sq
-    //                 }
-    //                 pseudo-legal => {
-    //                         let max_idx = values.iter().enumerate().fold(0, |acc, (i, x)| {
-    //                                 if x > &values[acc] { i } else { acc }
-    //                         });
-    //                         ChessSquare::from(max_idx)
-    //                 }
-    //         }
+    out
 }
 
 pub fn train<B: AutodiffBackend>(
     model: ChessTransformer<B>,
-    optimizer: &mut OptimizerAdaptor<Adam, ChessTransformer<B>, B>, // why is this so fukin cursed
+    optimizer: &mut OptimizerAdaptor<Adam, ChessTransformer<B>, B>, // what... tf?
     config: TrainingConfig,
     games: &ReplayBuffer,
     device: &B::Device,
@@ -152,148 +244,3 @@ pub fn train<B: AutodiffBackend>(
     let grads = GradientsParams::from_grads(grads, &model);
     optimizer.step(config.learning_rate, model, grads);
 }
-// loop {
-//         let tensors = remaining_batch.iter_mut().for_each(|game| {
-//                 let board, meta = if game.side_to_move == black {
-//                         game.board.flip, game.meta.to_tensor
-//                 } else { game.board, game.meta}
-//
-//                 let board_tensor = board.to_tensor
-//                 let meta_tensor = meta.to_tensor + Tensor::from(half_move_clock/50)
-//
-//                 (board_tensor, meta_tensor)
-//         })
-//         if masking, get legal from squares mask output. gen legal from sqs, -> tensor where 0 -> f32::MIN, apply onto policy head, then softmax.
-//         if not masking, just skip that
-//         let outputs = transformer.forward(board_tensor, meta_tensor) // replace with search tree - take value from depth, but obviously next policy/moves_left
-//         // unbatch and turn into Vec<(policy: [f32;64], value: f32); batch_size>
-//
-//
-//         batch.iter_mut().for_each(|game| {
-//                 if let Some(entry) = game.GameStateEntry.last_mut() {
-//                         entry.value = value
-//                 }
-//                 game.GameStateEntry.push(GameStateEntry::new(..policy_sq))
-//         })
-//
-//         make tensors again, but include from_sq from last policy
-//
-//         do forward pass again
-//         populate entries
-//
-//         if needs promotion square, make tensors again {
-//                 forward pass again
-//                 extract piece:
-//                 map policy_sq {
-//                         to_sq = q
-//                         to_sq - 8 = r // square below promotion sq
-//                         to_sq - 16 = b // 2 squares below
-//                         to_sq - 24 = n // 3 below
-//                         _ => lose if pseudo legal or softmax it out
-//                 }
-//                 // promotion happen on same file so do like this
-//                 populate entries
-//         }
-//
-//         remaining_batch.map(|game| game.make_move) // this should fill in rest of game.GameStateEntry
-// }
-//
-// loop {
-//     ChessGame::fen_to_ascii(&game.to_fen());
-//     println!("{:?}'s turn.", game.side_to_move);
-//
-//     print!("Enter move (e.g., e2e4): ");
-//     io::stdout().flush().unwrap();
-//
-//     let mut input = String::new();
-//     io::stdin().read_line(&mut input).unwrap();
-//     let input = input.trim().to_lowercase();
-//
-//     if input == "quit" || input == "exit" {
-//         break;
-//     }
-//
-//     if input == "debug" {
-//         println!("{game:?}");
-//     }
-//
-//     let input = game.uci_to_move(&input);
-//     match input {
-//         Ok(input) => game.make_move(&input),
-//         Err(e) => println!("{e}"),
-//     }
-// }
-//
-// impl<B: Backend> SelfPlayRunner<B> {
-//     pub fn new(model: ChessTransformer<B>, device: B::Device) -> Self {
-//         Self { model, device }
-//     }
-//
-//     pub fn collect_data(&mut self, games: &mut Vec<ChessGame>, buffer: &mut ReplayBuffer, masking_enabled: bool) {
-//         let (boards, meta) = crate::bridge::games_to_tensors(games, &self.device);
-//         let (policy_logits, _values) = self.model.forward(boards, meta);
-//         let logits_data: Vec<Vec<f32>> = policy_logits.to_data().convert().value;
-//
-//         for (i, game) in games.iter_mut().enumerate() {
-//             if matches!(game.outcome, Outcome::Finished(_)) {
-//                 *game = ChessGame::default();
-//                 continue;
-//             }
-//
-//             let mut logits = logits_data[i].clone();
-//
-//             // Generate legal moves to check validity
-//             game.generate_pseudolegal();
-//             let legal_from_squares = game.get_possible_from_squares();
-//
-//             // --- EXPERIMENT LOGIC ---
-//             if masking_enabled {
-//                 // MASKING: Set illegal moves to -Infinity so Softmax makes them 0% prob
-//                 for sq_idx in 0..64 {
-//                     let sq = ChessSquare::new(sq_idx as u8).unwrap();
-//                     if !legal_from_squares.contains(&sq) {
-//                         logits[sq_idx] = f32::MIN;
-//                     }
-//                 }
-//             }
-//             // IF PUNISHMENT: We do nothing to logits. The model might pick an illegal square.
-//
-//             // Softmax & Sampling
-//             let probs = softmax(&logits);
-//             let selected_idx = sample_index(&probs);
-//             let selected_sq = ChessSquare::new(selected_idx as u8).unwrap();
-//
-//             // Check Legality
-//             let is_legal = legal_from_squares.contains(&selected_sq);
-//
-//             // Save Sample
-//             let (board_f32, meta_f32) = game.to_f32();
-//
-//             // We don't know the true value yet (Win/Loss), so we use a placeholder.
-//             // In a real engine, you backpropagate the result later.
-//             // For this specific experiment, immediate punishment is key.
-//             let mut reward = 0.0;
-//
-//             if !is_legal {
-//                 // PUNISHMENT: Immediate Loss
-//                 reward = -1.0;
-//                 game.outcome = Outcome::Finished(Some(game.side_to_move.opposite()));
-//             } else {
-//                 // LEGAL: Make the move (Random 'To' square for this simplified example)
-//                 let possible_tos = game.get_possible_to_squares(&selected_sq);
-//                 if let Some(to_sq) = possible_tos.choose(&mut rand::thread_rng()) {
-//                     let mv = crate::ChessMove::new(selected_sq, *to_sq, None);
-//                     game.make_move(&mv);
-//                     game.outcome = game.check_game_state();
-//                 }
-//             }
-//
-//             buffer.push(TrainingSample {
-//                 board: board_f32,
-//                 meta: meta_f32,
-//                 target_policy: selected_idx,
-//                 target_value: reward,
-//             });
-//         }
-//     }
-// }
