bcefddd121bf661e10b0140e1e26ca4c0599c1e6
fqian <francois.qian2@gmail.com>
2026-04-07 06:08:26 +0100

    bugfix + added metrics

--- src/engine.rs
+++ src/engine.rs
@@ -34,9 +34,11 @@ pub struct TrainingConfig {
     pub masked: bool,
     pub legal: bool,
     pub optimizer: AdamConfig,
-    #[config(default = 10)]
+    #[config(default = 5)]
     pub num_epochs: usize,
     #[config(default = 1024)]
+    pub steps_per_iter: usize,
+    #[config(default = 256)]
     pub batch_size: usize,
     #[config(default = 4)]
     pub num_workers: usize,
@@ -55,13 +57,16 @@ pub fn model_make_outputs<B: Backend>(
 ) -> Vec<NetworkLabels> {
     let batch_size = config.batch_size;
     let (boards, metas) = inputs_to_tensor(inputs, device);
-    let (mut policies, values) = model.forward(boards, metas);
+    let (mut policies, mut values) = model.forward(boards, metas);
     if let Some(masks) = masks {
         let mask_data = TensorData::new(masks, [batch_size, 64]);
         let mask = Tensor::<B, 2, Bool>::from_data(mask_data, device);
+        let mask = mask.bool_not();
+
         policies = policies.clone().mask_fill(mask, -1e9);
     }
     policies = softmax(policies, 1);
+    values = softmax(values, 1);
     // batch_size x 64
     let policies: Tensor<B, 1> = policies.flatten(0, 1);
     let policies = policies.into_data().to_vec::<f32>().unwrap();
@@ -107,76 +112,120 @@ pub fn play<B: AutodiffBackend>(artifact_dir: &str, mcts_config: &MctsConfig, tr
     create_artifact_dir(artifact_dir);
     B::seed(device, training_config.seed);
 
-    let model: ChessTransformer<B> = training_config.model.init(device);
-    let mut replay_buffer = ReplayBuffer::new(10000);
+    let mut model: ChessTransformer<B> = training_config.model.init(device);
+    let mut replay_buffer = ReplayBuffer::new(256000);
     let mut optimizer = training_config.optimizer.init::<B, ChessTransformer<B>>();
     let mut games = vec![ChessGame::default(); training_config.batch_size];
-    let mut mctss: Vec<Mcts> = games.iter().map(|game| Mcts::from_game(&game, 1000, *mcts_config)).collect();
+    let mut mctss: Vec<Mcts> = games.iter().map(|game| Mcts::from_game(&game, 1024, *mcts_config)).collect();
     let mut rng = SmallRng::seed_from_u64(training_config.seed);
     let recorder = NamedMpkFileRecorder::<FullPrecisionSettings>::new();
 
+    let csv_path = format!("{}/metrics.csv", artifact_dir);
+    let mut csv_file = OpenOptions::new().create(true).append(true).open(&csv_path).unwrap();
+    if std::fs::metadata(&csv_path).unwrap().len() == 0 {
+        writeln!(csv_file, "iteration,avg_loss,avg_game_length,unique_positions,avg_illegal_prob").unwrap();
+    }
+
     let mut iterations = 0;
+    let mut positions_expanded: u32 = 0;
+    let mut game_over_count: f32 = 0.0;
+    let mut average_game_length: f32 = 0.0;
     loop {
         info!("Starting Self play - Train loop: cycle {}", iterations);
 
-        for _ in 0..training_config.num_epochs {
-            games.iter_mut().zip(mctss.iter_mut()).for_each(|(game, mcts)| {
-                if let Outcome::Finished(_) = game.check_game_state(training_config.legal) {
-                    info!("Game over detected, Starting new game");
-                    *game = ChessGame::default();
-                    *mcts = Mcts::from_game(&game, 1000, *mcts_config);
-                }
-            });
+        let mut illegal_move_weight: f64 = 0.0;
 
-            for count in 0..mcts_config.num_simulations {
-                mctss.iter_mut().for_each(|e| {
-                    while e.path.is_none() {
-                        e.traverse();
+        for _ in 0..training_config.steps_per_iter {
+            let (total_length, total_games): (f32, f32) = games
+                .par_iter_mut()
+                .zip(mctss.par_iter_mut())
+                .map(|(game, mcts)| {
+                    if let Outcome::Finished(_) = game.check_game_state(training_config.legal) {
+                        let length = game.game_history.len() as f32;
+                        *game = ChessGame::default();
+                        *mcts = Mcts::from_game(&game, 1024, *mcts_config);
+                        return (length, 1.0);
                     }
+                    (0.0, 0.0)
+                })
+                .reduce(|| (0.0, 0.0), |a, b| (a.0 + b.0, a.1 + b.1));
+
+            average_game_length += total_length;
+            game_over_count += total_games;
+
+            for _count in 0..mcts_config.num_simulations {
+                mctss.par_iter_mut().for_each(|mcts| {
+                    mcts.traverse_get_terminal();
                 });
-                expand_batch(&mut mctss[..], model.clone(), training_config, device);
+                let (unique, weight) = expand_batch(&mut mctss[..], model.clone().valid(), training_config, device);
+                illegal_move_weight += weight;
+                positions_expanded += unique;
             }
-            info!("Mcts done simulating");
 
             // get best move and play it
-            mctss.iter_mut().zip(games.iter_mut()).for_each(|(mcts, game)| {
-                let sample = mcts.make_targets();
+            let new_samples: Vec<_> = mctss
+                .par_iter_mut()
+                .zip(games.par_iter_mut())
+                .map(|(mcts, game)| {
+                    let sample = mcts.make_targets();
+                    if let Some(mov) = mcts.get_move_to_play() {
+                        game.make_move(&mov);
+                        // info!("\n{}", game.position);
+                        info!("Selected move: {}", &mov.to_uci());
+                    };
+                    sample
+                })
+                .collect();
+
+            for sample in new_samples {
                 replay_buffer.push(sample);
-                if let Some(mov) = mcts.get_move() {
-                    game.make_move(&mov);
-                    info!("\n{}", game.position);
-                    info!("Selected move: {}", &mov.to_uci());
-                } else {
-                    info!("\n{}", game.position);
-                    info!("No move to play");
-                }
-            });
+            }
+
+            // info!("Replay Buffer size: {}", replay_buffer.buffer.len());
         }
 
+        let mut loss_val: f32 = 0.0;
         for epoch in 0..training_config.num_epochs {
             info!("Training model: epoch {}", epoch);
-            train(&model, &mut optimizer, &training_config, &replay_buffer, device, &mut rng);
+            let (new_model, val) = train(model.clone(), &mut optimizer, &training_config, &replay_buffer, device, &mut rng);
+            model = new_model;
+            loss_val += val;
         }
+        loss_val /= training_config.num_epochs as f32;
 
-        info!("Saving model snapshot at: {}", &artifact_dir);
-        let _ = model.clone().save_file(artifact_dir, &recorder);
+        let total_batches = (training_config.steps_per_iter * mcts_config.num_simulations) as f64;
+        let avg_illegal_prob = illegal_move_weight / total_batches;
+
+        writeln!(csv_file, "{},{},{},{},{}", iterations, loss_val, average_game_length / game_over_count, positions_expanded, avg_illegal_prob)
+            .unwrap();
+        csv_file.flush().unwrap();
+
+        if iterations % 10 == 0 {
+            let snapshot_path = format!("{}/model-{}", artifact_dir, iterations / 10);
+            info!("Saving model snapshot at: {}", &snapshot_path);
+            if let Err(err) = model.clone().save_file(snapshot_path, &recorder) {
+                println!("failed to save model: {}", err);
+            }
+        }
 
         iterations += 1;
     }
 }
 
 pub fn train<B: AutodiffBackend>(
-    model: &ChessTransformer<B>,
-    optimizer: &mut OptimizerAdaptor<Adam, ChessTransformer<B>, B>, // what... tf?
+    model: ChessTransformer<B>,
+    optimizer: &mut OptimizerAdaptor<Adam, ChessTransformer<B>, B>,
     config: &TrainingConfig,
     games: &ReplayBuffer,
     device: &B::Device,
     rng: &mut SmallRng,
-) {
+) -> (ChessTransformer<B>, f32) {
     let datas: ChessBatch<B> = games.sample_batch(config.batch_size, rng, device);
     let output = model.forward_classification(datas);
     let loss = output.loss;
     let grads = loss.backward();
-    let grads = GradientsParams::from_grads(grads, model);
-    optimizer.step(config.learning_rate, model.clone(), grads);
+    let grads = GradientsParams::from_grads(grads, &model);
+
+    let loss_val = loss.clone().into_data().to_vec::<f32>().unwrap()[0];
+    (optimizer.step(config.learning_rate, model.clone(), grads), loss_val)
 }
