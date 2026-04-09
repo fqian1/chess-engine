1ade18a4a046cd7a9bb0d27fd68c5034f2267401
fqian <francois.qian2@gmail.com>
2026-04-09 03:20:11 +0100

    sync

--- run.sh
+++ run.sh
@@ -17,17 +17,19 @@ run | r)
 build_nvidia | bn)
     cargo build --release --no-default-features --features cuda
     ;;
+quick|q)
+    ./target/release/chess-engine -p "./tmp" -b 8 -n 80 -i 8 -e 40
 legal_masked|lm)
-    ./target/release/chess-engine -l -m -p "~/chess-engine-stats/legal_masked/"
+    ./target/release/chess-engine -l -m -p "/scratch/fq00038-legal_masked/"
     ;;
 legal_unmasked|lu)
-    ./target/release/chess-engine -l -p "~/chess-engine-stats/legal_unmasked/"
+    ./target/release/chess-engine -l -p "/scratch/fq00038-legal_unmasked/"
     ;;
 pseudo_legal_masked|pm)
-    ./target/release/chess-engine -m -p "~/chess-engine-stats/pseudo_legal_masked/"
+    ./target/release/chess-engine -m -p "/scratch/fq0038-pseudo_legal_masked/"
     ;;
 pseudo_legal_unmasked|pu)
-    ./target/release/chess-engine -p "~/chess-engine-stats/pseudo_legal_unmasked/"
+    ./target/release/chess-engine -p "/scratch/fq00038-pseudo_legal_unmasked/"
     ;;
 *)
     echo "Usage: $0 {test|t|debug|d|run|r}"
