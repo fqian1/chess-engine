4b68a453c394efcfa8625f0f0da021ecdc30e90e
fqian <francois.qian2@gmail.com>
2026-04-23 11:22:57 +0100

    tune hyperparams for adamw

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,7 @@ fn main() {
     let d_ff = 4 * d_model;
 
     let model_config = ChessTransformerConfig::new(d_model, n_heads, d_ff, n_layers);
-    let optimizer_config = AdamWConfig::new().with_beta_1(0.9).with_beta_2(0.999).with_weight_decay(1e-4);
+    let optimizer_config = AdamWConfig::new().with_beta_1(0.9).with_beta_2(0.99).with_weight_decay(1e-4);
     let scheduler_config = NoamLrSchedulerConfig::new(0.01).with_model_size(512);
 
     let training_config = TrainingConfig {
